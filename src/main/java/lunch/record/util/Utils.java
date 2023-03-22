d560af1e5b869481fb26a5303821503de04e1eae
slogan-choi <slogan.choi@gmail.com>
2023-03-22 17:13:09 +0900

    feat: 유틸 추가

---
+++ src/main/java/lunch/record/util/Utils.java
@@ -0,0 +1,55 @@
+package lunch.record.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Base64Utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Slf4j
+public class Utils {
+
+    public static byte[] imageToByteArray(String filePath) {
+        byte[] returnValue = null;
+
+        ByteArrayOutputStream baos = null;
+        FileInputStream fis = null;
+
+        try {
+            baos = new ByteArrayOutputStream();
+            fis = new FileInputStream(filePath);
+
+            byte[] buf = new byte[1024];
+            int read = 0;
+
+            while ((read = fis.read(buf, 0, buf.length)) != -1) {
+                baos.write(buf, 0, read);
+            }
+            returnValue = baos.toByteArray();
+        } catch (IOException e) {
+            log.error("io error", e);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    log.info("error", e);
+                }
+            }
+
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    log.info("error", e);
+                }
+            }
+        }
+        return returnValue;
+    }
+
+    public static String getBase64EncodeByte(byte[] content){
+        return Base64Utils.encodeToString(content);
+    }
+}
